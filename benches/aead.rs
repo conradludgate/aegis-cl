979751d4273d9ba22a6cc6568c0583e77c077086
Conrad Ludgate <conradludgate@gmail.com>
2025-04-17 19:29:35 +0100

    switch to divan

---
+++ benches/aead.rs
@@ -0,0 +1,186 @@
+fn main() {
+    divan::Divan::from_args()
+        .sample_size(1000)
+        .sample_count(1000)
+        .main();
+}
+
+mod libaegis {
+    use divan::Bencher;
+    use divan::counter::BytesCount;
+
+    use std::hint::black_box;
+
+    use aegis::{
+        aegis128l::Aegis128L, aegis128x2::Aegis128X2, aegis128x4::Aegis128X4, aegis256::Aegis256,
+        aegis256x2::Aegis256X2, aegis256x4::Aegis256X4,
+    };
+
+    #[divan::bench]
+    fn aegis128l(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 16];
+        let nonce = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis128L::<16>::new(&black_box(nonce), &black_box(key));
+            state.encrypt_in_place(black_box(&mut m), &[])
+        });
+    }
+    #[divan::bench]
+    fn aegis128x2(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 16];
+        let nonce = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis128X2::<16>::new(&black_box(nonce), &black_box(key));
+            state.encrypt_in_place(black_box(&mut m), &[])
+        });
+    }
+    #[divan::bench]
+    fn aegis128x4(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 16];
+        let nonce = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis128X4::<16>::new(&black_box(nonce), &black_box(key));
+            state.encrypt_in_place(black_box(&mut m), &[])
+        });
+    }
+    #[divan::bench]
+    fn aegis256(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 32];
+        let nonce = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis256::<16>::new(&black_box(nonce), &black_box(key));
+            state.encrypt_in_place(black_box(&mut m), &[])
+        });
+    }
+    #[divan::bench]
+    fn aegis256x2(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 32];
+        let nonce = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis256X2::<16>::new(&black_box(nonce), &black_box(key));
+            state.encrypt_in_place(black_box(&mut m), &[])
+        });
+    }
+    #[divan::bench]
+    fn aegis256x4(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 32];
+        let nonce = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis256X4::<16>::new(&black_box(nonce), &black_box(key));
+            state.encrypt_in_place(black_box(&mut m), &[])
+        });
+    }
+}
+
+mod aegis_cl {
+    use divan::Bencher;
+    use divan::counter::BytesCount;
+
+    use std::hint::black_box;
+
+    use aead::{AeadInOut, KeyInit, consts::U16, inout::InOutBuf};
+    use aegis_cl::{Aegis128L, Aegis128X2, Aegis128X4, Aegis256, Aegis256X2, Aegis256X4};
+
+    #[divan::bench]
+    fn aegis128l(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 16];
+        let nonce = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis128L::<U16>::new(&black_box(key).into());
+            state.encrypt_inout_detached(
+                &black_box(nonce).into(),
+                &[],
+                InOutBuf::from(black_box(&mut *m)),
+            )
+        });
+    }
+    #[divan::bench]
+    fn aegis128x2(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 16];
+        let nonce = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis128X2::<U16>::new(&black_box(key).into());
+            state.encrypt_inout_detached(
+                &black_box(nonce).into(),
+                &[],
+                InOutBuf::from(black_box(&mut *m)),
+            )
+        });
+    }
+    #[divan::bench]
+    fn aegis128x4(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 16];
+        let nonce = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis128X4::<U16>::new(&black_box(key).into());
+            state.encrypt_inout_detached(
+                &black_box(nonce).into(),
+                &[],
+                InOutBuf::from(black_box(&mut *m)),
+            )
+        });
+    }
+    #[divan::bench]
+    fn aegis256(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 32];
+        let nonce = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis256::<U16>::new(&black_box(key).into());
+            state.encrypt_inout_detached(
+                &black_box(nonce).into(),
+                &[],
+                InOutBuf::from(black_box(&mut *m)),
+            )
+        });
+    }
+    #[divan::bench]
+    fn aegis256x2(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 32];
+        let nonce = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis256X2::<U16>::new(&black_box(key).into());
+            state.encrypt_inout_detached(
+                &black_box(nonce).into(),
+                &[],
+                InOutBuf::from(black_box(&mut *m)),
+            )
+        });
+    }
+    #[divan::bench]
+    fn aegis256x4(b: Bencher) {
+        let mut m = vec![0xd0u8; 16384];
+        let key = [0u8; 32];
+        let nonce = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let state = Aegis256X4::<U16>::new(&black_box(key).into());
+            state.encrypt_inout_detached(
+                &black_box(nonce).into(),
+                &[],
+                InOutBuf::from(black_box(&mut *m)),
+            )
+        });
+    }
+}
---
+++ benches/mac.rs
@@ -0,0 +1,165 @@
+fn main() {
+    divan::Divan::from_args()
+        .sample_size(1000)
+        .sample_count(1000)
+        .main();
+}
+
+mod libaegis {
+    use divan::Bencher;
+    use divan::counter::BytesCount;
+
+    use std::hint::black_box;
+
+    use aegis::{
+        aegis128l::Aegis128LMac, aegis128x2::Aegis128X2Mac, aegis128x4::Aegis128X4Mac,
+        aegis256::Aegis256Mac, aegis256x2::Aegis256X2Mac, aegis256x4::Aegis256X4Mac,
+    };
+
+    #[divan::bench]
+    fn aegis128l(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = Aegis128LMac::<16>::new(&black_box(key));
+            state.update(black_box(&m));
+            black_box(state.finalize());
+        });
+    }
+    #[divan::bench]
+    fn aegis128x2(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = Aegis128X2Mac::<16>::new(&black_box(key));
+            state.update(black_box(&m));
+            state.finalize()
+        });
+    }
+    #[divan::bench]
+    fn aegis128x4(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = Aegis128X4Mac::<16>::new(&black_box(key));
+            state.update(black_box(&m));
+            state.finalize()
+        });
+    }
+    #[divan::bench]
+    fn aegis256(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = Aegis256Mac::<16>::new(&black_box(key));
+            state.update(black_box(&m));
+            state.finalize()
+        });
+    }
+    #[divan::bench]
+    fn aegis256x2(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = Aegis256X2Mac::<16>::new(&black_box(key));
+            state.update(black_box(&m));
+            state.finalize()
+        });
+    }
+    #[divan::bench]
+    fn aegis256x4(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = Aegis256X4Mac::<16>::new(&black_box(key));
+            state.update(black_box(&m));
+            state.finalize()
+        });
+    }
+}
+
+mod aegis_cl {
+    use divan::Bencher;
+    use divan::counter::BytesCount;
+
+    use std::hint::black_box;
+
+    use aegis_cl::{
+        AegisMac128L, AegisMac128X2, AegisMac128X4, AegisMac256, AegisMac256X2, AegisMac256X4,
+    };
+    use digest::{Mac, crypto_common::KeyIvInit};
+    use hybrid_array::sizes::U16;
+
+    #[divan::bench]
+    fn aegis128l(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = AegisMac128L::<U16>::new(&black_box(key).into(), &[0u8; 16].into());
+            state.update(black_box(&m));
+            black_box(state.finalize());
+        });
+    }
+    #[divan::bench]
+    fn aegis128x2(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = AegisMac128X2::<U16>::new(&black_box(key).into(), &[0u8; 16].into());
+            state.update(black_box(&m));
+            state.finalize()
+        });
+    }
+    #[divan::bench]
+    fn aegis128x4(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 16];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = AegisMac128X4::<U16>::new(&black_box(key).into(), &[0u8; 16].into());
+            state.update(black_box(&m));
+            state.finalize()
+        });
+    }
+    #[divan::bench]
+    fn aegis256(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = AegisMac256::<U16>::new(&black_box(key).into(), &[0u8; 32].into());
+            state.update(black_box(&m));
+            state.finalize()
+        });
+    }
+    #[divan::bench]
+    fn aegis256x2(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = AegisMac256X2::<U16>::new(&black_box(key).into(), &[0u8; 32].into());
+            state.update(black_box(&m));
+            state.finalize()
+        });
+    }
+    #[divan::bench]
+    fn aegis256x4(b: Bencher) {
+        let m = vec![0xd0u8; 65536];
+        let key = [0u8; 32];
+
+        b.counter(BytesCount::of_slice(&m)).bench_local(|| {
+            let mut state = AegisMac256X4::<U16>::new(&black_box(key).into(), &[0u8; 32].into());
+            state.update(black_box(&m));
+            state.finalize()
+        });
+    }
+}
